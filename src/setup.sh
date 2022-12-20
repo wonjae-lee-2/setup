43c4d30927296cf5d12a944b010bcf252b77cbcb
Ubuntu <wonjae.lee.2@gmail.com>
2022-12-20 16:00:34 +0000

    fix: update directories to scripts

--- src/setup.sh
+++ src/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set folder paths.
-export SRC_FOLDER=~/workspaces/vm/src
+export SRC_FOLDER=~/workspaces/setup/src
 export COMMANDS_FOLDER=$SRC_FOLDER/commands
 export RESOURCES_FOLDER=$SRC_FOLDER/resources
-export CONFIG_FOLDER=~/.vm
+export CONFIG_FOLDER=~/.setup
 export DOWNLOAD_FOLDER=~/downloads
 
 # Run the script identified by positional arguments.
