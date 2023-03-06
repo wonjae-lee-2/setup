962780785f8c9e99485049bfd5ba4cc962bdd3e5
Ubuntu <wonjae.lee.2@gmail.com>
2023-03-06 10:10:04 +0000

    fix: check if directories exist before making them

--- src/commands/config/init.sh
+++ src/commands/config/init.sh
@@ -16,7 +16,9 @@ read -p "Which version of Spark would you like to install? " SPARK_VERSION
 read -p "Which version of Java compatible with Spark would you like to install? " JAVA_VERSION
 
 # Create the config folder. (CONFIG_FOLDER is exported from 'setup.sh'.)
-mkdir $CONFIG_FOLDER
+if [ ! -d "$CONFIG_FOLDER" ]; then
+    mkdir $CONFIG_FOLDER
+fi
 
 # Update and save the env script to the config folder. (RESOURCES_FOLDER is exported from 'setup.sh'.)
 sed -e "s/PYTHON_VERSION=/PYTHON_VERSION=$PYTHON_VERSION/g" \
@@ -27,7 +29,9 @@ sed -e "s/PYTHON_VERSION=/PYTHON_VERSION=$PYTHON_VERSION/g" \
     $RESOURCES_FOLDER/env.sh > $CONFIG_FOLDER/env.sh
 
 # Create the vscode config folder. (VSCODE_CONFIG_FOLDER is exported from 'setup.sh'.)
-mkdir $VSCODE_CONFIG_FOLDER
+if [ ! -d "$VSCODE_CONFIG_FOLDER" ]; then
+    mkdir $VSCODE_CONFIG_FOLDER
+fi
 
 # Copy the settings file to the vscode config folder. (RESOURCES_FOLDER is exported from 'setup.sh'.)
 cp $RESOURCES_FOLDER/settings.json $VSCODE_CONFIG_FOLDER/settings.json
