6a7f95a5bdc7328ec7ee3b70f6f57910e3fedfdd
Ubuntu <wonjae.lee.2@gmail.com>
2023-03-06 13:32:37 +0000

    fix: change the url to posit package manager

--- src/resources/requirements.r
+++ src/resources/requirements.r
@@ -1,9 +1,9 @@
 project_folder <- c(Sys.getenv("PROJECT_FOLDER"))
 linux_codename <- c(Sys.getenv("LINUX_CODENAME"))
-rspm_url <- paste("https://packagemanager.rstudio.com/all/__linux__/", linux_codename, "/latest", sep = "")
+rspm_url <- paste("https://packagemanager.posit.co/cran/__linux__/", linux_codename, "/latest", sep = "")
 package_list <- c("sparklyr") # https://cloud.r-project.org/web/packages/sparklyr/index.html
 
-options(repos = c(RSPM = rspm_url)) # https://packagemanager.rstudio.com/client/#/repos/1/overview
+options(repos = c(CRAN = rspm_url)) # https://packagemanager.posit.co/client/#/repos/2/overview
 dir.create(Sys.getenv("R_LIBS_USER"), recursive = TRUE)
 .libPaths(c(Sys.getenv("R_LIBS_USER"), .libPaths()))
 install.packages("renv") # https://cloud.r-project.org/web/packages/renv/index.html
