2ae1c00170bc67cac3105891b22f0a83620f90f8
Ubuntu <wonjae.lee.2@gmail.com>
2023-03-08 12:22:21 +0000

    fix: set the project directory manually

--- src/resources/install.r
+++ src/resources/install.r
@@ -1,6 +1,7 @@
+project_folder <- c(Sys.getenv("PROJECT_FOLDER"))
 package_list <- c("sparklyr", "devtools") # https://cloud.r-project.org/web/packages/sparklyr/index.html
 
-renv::activate()
+renv::activate(project = project_folder)
 options(repos = c(CRAN = "https://packagemanager.posit.co/cran/__linux__/jammy/latest"))
 install.packages("remotes")
 remotes::install_cran(package_list)
