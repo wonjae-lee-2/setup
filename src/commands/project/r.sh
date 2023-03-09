db89174825a2afddc0b76c8fcdc427629242da71
Wonjae Lee <wonjae.lee.2@gmail.com>
2023-03-09 04:45:57 +0000

    feat: initialize an R project

---
+++ src/commands/project/r.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Copy the python script template to the current directory. (CURRENT_FOLDER is exported from 'setup.sh'.)
+cp -r $RESOURCES_FOLDER/templates/r/$THIRD_ARGUMENT/* $CURRENT_FOLDER
