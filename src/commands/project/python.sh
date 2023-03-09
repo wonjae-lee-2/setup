b8afcffffee552092b46357a68a28de852bd780b
Ubuntu <wonjae.lee.2@gmail.com>
2023-03-09 03:43:28 +0000

    fix: use the third argument without case statement

--- src/commands/project/python.sh
+++ src/commands/project/python.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-
-case $THIRD_ARGUMENT in # THIRD_ARGUMENT is exported from 'setup.sh'.
-    # Copy the python script template to the current directory. (CURRENT_FOLDER is exported from 'setup.sh'.)
-    "script") cp -r $RESOURCES_FOLDER/templates/python/package/* $CURRENT_FOLDER
-    ;;
-esac
+# Copy the python script template to the current directory. (CURRENT_FOLDER is exported from 'setup.sh'.)
+cp -r $RESOURCES_FOLDER/templates/python/$THIRD_ARGUMENT/* $CURRENT_FOLDER
