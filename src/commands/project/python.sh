fa6ed2a1144d239547b5f6c6a26ba114c789f9ac
Ubuntu <wonjae.lee.2@gmail.com>
2023-03-09 03:16:47 +0000

    feat: use the third argument to choose templates

--- src/commands/project/python.sh
+++ src/commands/project/python.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# Copy the python project template to the current directory. (CURRENT_FOLDER is exported from 'setup.sh'.)
-cp -r $RESOURCES_FOLDER/templates/python/* $CURRENT_FOLDER
+
+case $THIRD_ARGUMENT in # THIRD_ARGUMENT is exported from 'setup.sh'.
+    # Copy the python script template to the current directory. (CURRENT_FOLDER is exported from 'setup.sh'.)
+    "script") cp -r $RESOURCES_FOLDER/templates/python/package/* $CURRENT_FOLDER
+    ;;
+esac
--- src/setup.sh
+++ src/setup.sh
@@ -10,5 +10,6 @@ export VSCODE_CONFIG_FOLDER=~/.vscode-server/data/Machine
 export CURRENT_FOLDER=$(pwd)
 
 # Run the script identified by positional arguments.
+export THIRD_ARGUMENT=$3
 cd $COMMANDS_FOLDER/$1
 bash $2.sh
