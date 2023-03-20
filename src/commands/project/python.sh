7af3ce462161af413b923d9420adf4652a27a3dd
Wonjae Lee <wonjae.lee.2@gmail.com>
2023-03-20 02:04:28 +0000

    fix: copy the directory instead of files

--- src/commands/project/python.sh
+++ src/commands/project/python.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Copy the python script template to the current directory. (CURRENT_FOLDER is exported from 'setup.sh'.)
-cp -r $RESOURCES_FOLDER/templates/python/$THIRD_ARGUMENT/* $CURRENT_FOLDER
+cp -r $RESOURCES_FOLDER/templates/python/$THIRD_ARGUMENT/. $CURRENT_FOLDER
