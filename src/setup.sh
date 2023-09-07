a2143e13c178b4e27bfa6f2b218b301461209dd9
Wonjae Lee <wonjae.lee.2@gmail.com>
2023-09-07 14:31:43 +0000

    fix: modify paths

--- src/setup.sh
+++ src/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Set folder paths.
-export SRC_FOLDER=~/workspaces/setup/src
+export SRC_FOLDER=~/workspace/setup/src
 export COMMANDS_FOLDER=$SRC_FOLDER/commands
 export RESOURCES_FOLDER=$SRC_FOLDER/resources
 export CONFIG_FOLDER=~/.setup
-export DOWNLOAD_FOLDER=~/downloads
+export DOWNLOAD_FOLDER=~/download
 export VSCODE_CONFIG_FOLDER=~/.vscode-server/data/Machine
 export CURRENT_FOLDER=$(pwd)
 
